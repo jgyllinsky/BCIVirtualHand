158096902da2e145b67b20c39c1413822dc145c9
jgyllinsky <jgyllinsky@users.noreply.github.com>
2019-07-20 12:15:33 -0400

    Update Quadrant_Hand_Signs.py
    Move code to main function, and add pygame without text output

--- demos/cursor_tasks/Quadrant_Hand_Signs.py
+++ demos/cursor_tasks/Quadrant_Hand_Signs.py
@@ -37,113 +37,124 @@
 
 ## Includes
 
-import socket
+
+import contextlib #< import pygame without extra text, alternatively {os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"} 
+with contextlib.redirect_stdout(None):
+	import pygame
+	from pygame import *
+	from pygame.locals import *
+	
+import time
+import os
 import sys
+import socket
+
+    
+    
+def main(args):
+
+	## Global Variables
+
+	### Networking Variables
+	UDP_IP_ADDRESS = '' #< Address used by UDP protocol; The blank string allows for a non-specific IP to bind to. This is useful if there are more than one network interfaces that the local computer identifies as.
+	UDP_PORT_NO = 8888 #< Port to bind to. Given the {UDP_IP_ADDRESS = ''}, this binds as *:8888
+
+	serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #< serverSocket to listen to for UDP messages
+	serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO)) #< Bind to it
+
+	# https://stackoverflow.com/questions/38478913/delay-when-receiving-udp-packets-in-python-in-real-time
+	#serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1) #< Maybe be useful
+	serverSock.settimeout(0.1)
+
+	data = '' #< Blank data for incoming stream
+	addr = '' #< Blank data for incoming stream
+	d = [b'','']
+		 
+	counter = 0
 
 
-## Global Variables
 
-### Networking Variables
-UDP_IP_ADDRESS = '' #> Address used by UDP protocol; The blank string allows for a non-specific IP to bind to. This is useful if there are more than one network interfaces that the local computer identifies as.
-UDP_PORT_NO = 8888 #> Port to bind to. Given the {UDP_IP_ADDRESS = ''}, this binds as *:8888
+	### Location of Cursor Variables
+	myX = 0
+	myY = 0
+	hand_state = "Q1"
 
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #> serverSocket to listen to for UDP messages
-serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO)) #> Bind to it
+	MAX_Y = 1
+	MAX_X = 0
+	MIN_Y = 1
+	MIN_X = 0
 
-# https://stackoverflow.com/questions/38478913/delay-when-receiving-udp-packets-in-python-in-real-time
-#serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1) #> Maybe be useful
-serverSock.settimeout(0.1)
-
-data = '' #> Blank data for incoming stream
-addr = '' #> Blank data for incoming stream
-d = [b'','']
-     
-counter = 0
-
-
-
-### Location of Cursor Variables
-myX = 0
-myY = 0
-hand_state = "Q1"
-
-MAX_Y = 1
-MAX_X = 0
-MIN_Y = 1
-MIN_X = 0
-
-while True:
-	print ('X,Y = ' + str(myX) + ' , ' + str(myY)+ ' (' + hand_state +') at loop #: ' +str(counter))
-	try:
-		d = serverSock.recvfrom(1024)
-	except socket.timeout:
-		print ('caught a timeout...')
-		#continue
-		continue
 		
-	if(d):
-		data = d[0]
-		addr = d[1]
-		counter = counter+1 
-	 
-		if not data: 
-			# Something unhappy happened.
-			break
-	 
-		reply = 'OK, Data was..' + str(data)
-	 
-		reply = reply.encode()
-		serverSock.sendto(reply , addr)
-
-		val_d =  data.strip()
-
-		val = val_d
-
-
-		if( 'CursorPosX'.encode() in val ):
-			obj = val.decode().lstrip().split()
-			myX = int(obj[1])
-			#print 'X = ' + str(myX)
-		if( 'CursorPosY'.encode() in val ):
-			obj = val.decode().lstrip().split()
-			myY = int(obj[1])
-	
-
-	
-		if(myX > MAX_X):
-			MAX_X = myX
-	
-		if(myY > MAX_Y):
-			MAX_Y = myY
+	while True:
+		print ('X,Y = ' + str(myX) + ' , ' + str(myY)+ ' (' + hand_state +') at loop #: ' +str(counter))
+		try:
+			d = serverSock.recvfrom(1024)
+		except socket.timeout:
+			print ('caught a timeout...')
+			#continue
+			continue
 			
-		if(myX < MAX_X):
-			MIN_X = myX
-	
-		if(myY < MAX_Y):
-			MIN_Y = myY
+		if(d):
+			data = d[0]
+			addr = d[1]
+			counter = counter+1 
+		 
+			if not data: 
+				# Something unhappy happened.
+				break
+		 
+			reply = 'OK, Data was..' + str(data)
+		 
+			reply = reply.encode()
+			serverSock.sendto(reply , addr)
 
-	
-		if((myX) <= (MAX_X/2)):
-			if((myY) <= (MAX_Y/2)):
-				hand_state = "QII"
+			val_d =  data.strip()
+
+			val = val_d
+
+
+			if( 'CursorPosX'.encode() in val ):
+				obj = val.decode().lstrip().split()
+				myX = int(obj[1])
+				#print 'X = ' + str(myX)
+			if( 'CursorPosY'.encode() in val ):
+				obj = val.decode().lstrip().split()
+				myY = int(obj[1])
+		
+
+		
+			if(myX > MAX_X):
+				MAX_X = myX
+		
+			if(myY > MAX_Y):
+				MAX_Y = myY
+				
+			if(myX < MAX_X):
+				MIN_X = myX
+		
+			if(myY < MAX_Y):
+				MIN_Y = myY
+
+		
+			if((myX) <= (MAX_X/2)):
+				if((myY) <= (MAX_Y/2)):
+					hand_state = "QII"
+				else:
+					hand_state = "QIII"
 			else:
-				hand_state = "QIII"
-		else:
-			if((myY) <= (MAX_Y/2)):
-				hand_state = "QI"
-			else:
-				hand_state = "QIV"
-			
-			   
-serverSock.close()    
+				if((myY) <= (MAX_Y/2)):
+					hand_state = "QI"
+				else:
+					hand_state = "QIV"
+				
+				   
+	serverSock.close()    
 
-    
-    
-    
-# ~ def main(args):
-    # ~ return 0
+		
+	
+	return 0
 
-# ~ if __name__ == '__main__':
-    # ~ import sys
-    # ~ sys.exit(main(sys.argv))
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
 
